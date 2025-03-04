07eeffd77d27ace33401090f880ea100937da66f
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-03-04 07:48:01 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ell-iot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ell_iot/core_test.clj
@@ -0,0 +1,7 @@
+(ns ell-iot.core-test
+  (:require [clojure.test :refer :all]
+            [ell-iot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
